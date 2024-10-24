7dd15bcafb892c80a98a6948a891d8afc9b62b80
Shevket Dzhaparov <santehplast1@gmail.com>
2024-10-24 01:14:47 -0400

    dbank environment

--- src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
@@ -3,7 +3,6 @@ package co.wedevx.digitalbank.automation.ui.runners;
 
 import org.junit.platform.suite.api.*;
 
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
@@ -11,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("ui/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="co.wedevx.digitalbank.automation.ui.steps")
 @IncludeTags("Test")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/CucumberReports/report.json")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/CucumberReports/report.json")
 
 public class UiRegressionRunner {
 
--- src/test/java/co/wedevx/digitalbank/automation/ui/steps/Hooks.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/steps/Hooks.java
@@ -1,33 +1,138 @@
+//package co.wedevx.digitalbank.automation.ui.steps;
+
+//public class Hooks {
+//    public Hooks() {
+//
+//    }
+//
+//    WebDriver driver = Driver.getDriver();
+//
+//    @Before("not @Registration")
+//    public void the_user_on_dbank_homepage() {
+//
+//        getDriver().get("https://dbank-qa.wedevx.co/bank/login");
+//    }
+//
+//    @BeforeAll()
+//    public static void establishConnectionToDB() {
+//        DBUtils.establishConnection();
+//
+//    }
+//
+//
+//    @After()
+//    public void afterEachScenario(Scenario scenario) {
+//        Driver.takesScreenShot(scenario);
+//        Driver.closeDriver();
+//    }
+//
+//    @AfterAll()
+//    public static void closeConnectionToDB() {
+//        DBUtils.closeConnection();
+//    }
+//}
+
 package co.wedevx.digitalbank.automation.ui.steps;
 
 import co.wedevx.digitalbank.automation.ui.utils.DBUtils;
 import co.wedevx.digitalbank.automation.ui.utils.Driver;
 import io.cucumber.java.*;
-
-import static co.wedevx.digitalbank.automation.ui.utils.Driver.getDriver;
+import org.openqa.selenium.WebDriver;
 
 public class Hooks {
 
-    @Before("@Registration")
-    public void establishConnectionToDB() {
+    @BeforeAll()
+    public static void establishConnectionToDB() {
         DBUtils.establishConnection();
-
     }
 
     @Before("not @Registration")
     public void the_user_on_dbank_homepage() {
-
-        getDriver().get("https://dbank-qa.wedevx.co/bank/login");
+        WebDriver driver = Driver.getDriver(); // Получаем драйвер внутри метода
+        driver.get("https://dbank-qa.wedevx.co/bank/login");
     }
 
-    @After("not @NegativeRegistrationCases")
+    @After()
     public void afterEachScenario(Scenario scenario) {
         Driver.takesScreenShot(scenario);
         Driver.closeDriver();
     }
 
-    @After()
-    public  void closeConnectionToDB() {
+    @AfterAll()
+    public static void closeConnectionToDB() {
         DBUtils.closeConnection();
     }
 }
+
+
+//
+//import co.wedevx.digitalbank.automation.ui.utils.DBUtils;
+//import co.wedevx.digitalbank.automation.ui.utils.Driver;
+//import io.cucumber.java.*;
+//import org.openqa.selenium.WebDriver;
+//
+//public class Hooks {
+//    public Hooks() {
+//    }
+//
+//    WebDriver driver = Driver.getDriver();
+//
+//    @Before("not @Registration")
+//    public void the_user_is_on_dbank_homepage() {
+//        driver.get("https://dbank-qa.wedevx.co/bank/login");
+//    }
+//
+//    @BeforeAll()
+//    public static void establishConnectionToDB() {
+//        DBUtils.establishConnection();
+//    }
+//
+//    @After()
+//    public void afterEachScenario(Scenario scenario) {
+//        Driver.takesScreenShot(scenario);
+//        Driver.closeDriver();
+//    }
+//
+//    @AfterAll()
+//    public static void closeConnectionToDB() {
+//        DBUtils.closeConnection();
+//    }
+//}
+
+
+
+
+//MY ORIGINAL
+//package co.wedevx.digitalbank.automation.ui.steps;
+//
+//        import co.wedevx.digitalbank.automation.ui.utils.DBUtils;
+//        import co.wedevx.digitalbank.automation.ui.utils.Driver;
+//        import io.cucumber.java.*;
+//
+//        import static co.wedevx.digitalbank.automation.ui.utils.Driver.getDriver;
+//
+//public class Hooks {
+//
+//    @Before("@Registration")
+//    public void establishConnectionToDB() {
+//        DBUtils.establishConnection();
+//
+//    }
+//
+//    @Before() //"not @Registration"
+//    public void the_user_on_dbank_homepage() {
+//
+//        getDriver().get("https://dbank-qa.wedevx.co/bank/login");
+//    }
+//
+//    @After() //"not @NegativeRegistrationCases"
+//    public void afterEachScenario(Scenario scenario) {
+//        Driver.takesScreenShot(scenario);
+//        Driver.closeDriver();
+//    }
+//
+//    @After()
+//    public  void closeConnectionToDB() {
+//        DBUtils.closeConnection();
+//    }
+//}
