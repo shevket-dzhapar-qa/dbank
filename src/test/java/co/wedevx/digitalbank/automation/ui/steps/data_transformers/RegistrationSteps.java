69975721d2ce2040c84561bcd1c56f80c94710d7
Shevket Dzhaparov <santehplast1@gmail.com>
2024-10-24 01:56:12 -0400

    dbank environment

--- src/test/java/co/wedevx/digitalbank/automation/ui/steps/data_transformers/RegistrationSteps.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/steps/data_transformers/RegistrationSteps.java
@@ -14,20 +14,14 @@ import java.util.Map;
 import static co.wedevx.digitalbank.automation.ui.utils.Driver.getDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//public class RegistrationSteps {
-//    public RegistrationSteps() {
-//
-//    }
-//
-//    RegistrationPage registrationPage = new RegistrationPage(getDriver());
-//    List<Map<String, Object>> nextValList = new ArrayList<>();
 public class RegistrationSteps {
-    RegistrationPage registrationPage;
+    public RegistrationSteps() {
+
+    }
+
+    RegistrationPage registrationPage = new RegistrationPage(getDriver());
     List<Map<String, Object>> nextValList = new ArrayList<>();
 
-    public RegistrationSteps() {
-        registrationPage = new RegistrationPage(getDriver());
-    }
 
     @Given("User navigates to Digital Bank signup page")
         public void user_navigates_to_digital_bank_signup_page() {
