273de6c5e3a0012d627825461058cbd7c4e539e4
Shevket Dzhaparov <santehplast1@gmail.com>
2024-10-24 02:40:51 -0400

    dbank environment

--- src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
@@ -4,14 +4,13 @@ package co.wedevx.digitalbank.automation.ui.runners;
 import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ui/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="co.wedevx.digitalbank.automation.ui.steps")
 @IncludeTags("Test")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/CucumberReports/report.json")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/CucumberReports/report.json")
 
 public class UiRegressionRunner {
 
